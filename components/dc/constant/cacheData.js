8dfbaeee8160718e61d84f63830e211640a89169
Cox Herbert <88811637+CoxHerbert@users.noreply.github.com>
2025-10-17 14:50:10 +0800

    refactor: extract process model dialog fetch

--- components/dc/constant/cacheData.js
+++ components/dc/constant/cacheData.js
@@ -1,4 +1,33 @@
 import Api from '@/api/index';
+import http from '@/http/api.js';
+
+const getProcessModelList = (params) => {
+    return new Promise((resolve, reject) => {
+        http.request({
+            url: '/api/blade-workflow/design/model/list',
+            method: 'get',
+            params,
+        })
+            .then((res) => {
+                const { code, data } = res.data;
+                if (code === 200) {
+                    res.data.data.records = res.data.data.records.map((row) => {
+                        return {
+                            ...row,
+                            id: row.modelKey,
+                            id_: row.id,
+                        };
+                    });
+                    resolve(res);
+                } else {
+                    reject(res);
+                }
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
 
 export default {
     processModel: {
@@ -8,7 +37,7 @@ export default {
         title: '流程模型选择',
         placeholder: '请输入流程模型选择',
         submitTitle: '流程模型',
-        dialogGet: Api.pdp.pdpSetting.getProcessModelList,
+        dialogGet: getProcessModelList,
         column: [
             {
                 label: '名称',
